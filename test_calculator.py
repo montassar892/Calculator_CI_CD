cb9c499e226d5f159176ca2396b25bd2edafb071
unknown <montassarlabidi892@gmail.com>
2026-05-24 12:22:53 +0100

    first commit

---
+++ test_calculator.py
@@ -0,0 +1,18 @@
+from calculator import add, subtract, multiply, divide
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(10, 4) == 6
+
+def test_multiply():
+    assert multiply(3, 3) == 9
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(5, 0)
